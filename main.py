ec806f006bf2ea860167286346dd20eac0613172
sajeeth1009 <sajeeth.sadanand@gmail.com>
2020-05-17 13:32:58 +0200

    Introducing FLASK_PORT env variable

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     logger.info("Initialising Application: Video Service")
     logger.info("Host - " + str(os.getenv("FLASK_HOST", "0.0.0.0")))
     logger.info("Debug - True")
-    logger.info("Port - " + str(os.getenv("HOST_PORT", 5000)))
-    app.run(host=os.getenv("FLASK_HOST", "0.0.0.0"), debug=True, port=os.getenv("HOST_PORT", 5000))
+    logger.info("Port - " + str(os.getenv("FLASK_PORT", 5000)))
+    app.run(host=os.getenv("FLASK_HOST", "0.0.0.0"), debug=True, port=os.getenv("FLASK_PORT", 5000))
